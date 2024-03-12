d7a2badd6ee27e55573b2d093e3dd9367a632705
daihaitao <958471597@qq.com>
2024-03-12 18:56:35 +0800

    添加数据

--- src/main/test1.go
+++ src/main/test1.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"image"
 	"image/color"
 	"image/gif"
 	"io"
 	"math"
 	"math/rand"
-	"os"
-	"time"
 )
 
 var palette = []color.Color{color.White, color.Black}
@@ -22,8 +22,10 @@ func test1() {
 	// The sequence of images is deterministic unless we seed
 	// the pseudo-random number generator using the current time.
 	// Thanks to Randall McPherson for pointing out the omission.
-	rand.Seed(time.Now().UTC().UnixNano())
-	lissajous(os.Stdout)
+	//rand.Seed(time.Now().UTC().UnixNano())
+	//lissajous(os.Stdout)
+
+	test3()
 }
 
 func lissajous(out io.Writer) {
@@ -52,4 +54,59 @@ func lissajous(out io.Writer) {
 		anim.Image = append(anim.Image, img)
 	}
 	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
+func test2() {
+	x := 2
+
+	p := &x
+
+	fmt.Println("p", p)
+	fmt.Println(*p)
+
+	*p = 3
+
+	fmt.Println(p)
+	fmt.Println(*p)
+	fmt.Println(x)
+
+}
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
+func test3() {
+	//flag.Parse()
+	//fmt.Print(strings.Join(flag.Args(), *sep))
+	//if !*n {
+	//	fmt.Println()
+	//
+	//}
+	//p := new(int) // p, *int 类型, 指向匿名的 int 变量
+	////p := new(int)   // p, *int 类型, 指向匿名的 int 变量
+	//fmt.Println(*p) // "0"
+	//fmt.Println(p)  // "0"
+	//fmt.Println(&p) // "0"
+	//*p = 2          // 设置 int 匿名变量的值为 2
+	//fmt.Println(*p) // "2"
+	//fmt.Println(p)  // "2"
+
+	y := new(int)
+	*y = 1
+
+	p1 := new(bool)
+	fmt.Println(*p1)
+	*p1 = true
+	fmt.Println(*p1)
+}
+
+func test4() {
+
+}
+
+func gcd(x, y int) int {
+	for y != 0 {
+		x, y = y, x%y
+	}
+	return x
 }
