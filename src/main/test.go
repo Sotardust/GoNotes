22bf2fc822d018463db122b7574ca4ff490ded18
daihaitao <958471597@qq.com>
2024-03-15 09:56:43 +0800

    测试代码

--- src/main/test.go
+++ src/main/test.go
@@ -1,35 +1,15 @@
 package main
 
 import (
+	"GoNotes/src/demo1"
 	"bufio"
-	"fmt"
 	"os"
 )
 
 func main() {
 
-	test1()
+	demo1.Main1()
 
-	counts := make(map[string]int)
-	files := os.Args[1:]
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
-			f.Close()
-		}
-	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
-	}
 }
 
 func countLines(f *os.File, counts map[string]int) {
